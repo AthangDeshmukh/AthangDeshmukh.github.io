256896269ce5f30edb5e6ff057ba1110ecfcad10
AthangDeshmukh <athang.deshmukh@gmail.com>
2025-05-12 20:55:50 +0530

    update

--- a6/script.js
+++ a6/script.js
@@ -1,9 +1,9 @@
 function display() {
-    // Get the username and password values
+     
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    // Display the values in the bottom line
+    
     const output = document.getElementById("output");
     output.textContent = `Username: ${username}, Password: ${password}`;
 }

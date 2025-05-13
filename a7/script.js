72eae064234ee5334bea0539ffe16160fee7bb8c
AthangDeshmukh <athang.deshmukh@gmail.com>
2025-05-13 19:30:47 +0530

    update

---
+++ a7/script.js
@@ -0,0 +1,11 @@
+function addNumbers() { 
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+
+     
+    const sum = num1 + num2;
+
+    
+    const output = document.getElementById("output");
+    output.textContent = `Sum: ${sum}`;
+}

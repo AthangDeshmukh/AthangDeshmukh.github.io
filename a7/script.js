0479c113ffa4e98332e8db95734da52048d1c62a
AthangDeshmukh <athang.deshmukh@gmail.com>
2025-05-13 19:32:44 +0530

    update

--- a7/script.js
+++ a7/script.js
@@ -1,4 +1,4 @@
-function addNumbers() { 
+const addNumbers = () => {
     const num1 = parseFloat(document.getElementById("num1").value);
     const num2 = parseFloat(document.getElementById("num2").value);
 

3ae478f9ca71714802e98ad529416db70c2d8d58
AthangDeshmukh <athang.deshmukh@gmail.com>
2025-05-17 20:36:37 +0530

    update

--- a14/node.js
+++ a14/node.js
@@ -7,7 +7,7 @@ async function dispData() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
   console.log(data);
-  greet(); // Call greet after data is fetched and printed
+  greet(); 
 }
 
 dispData();

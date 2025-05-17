236ab5806775149371fd454ca0d26904782ada23
AthangDeshmukh <athang.deshmukh@gmail.com>
2025-05-17 19:06:17 +0530

    update

--- a13/script.js
+++ a13/script.js
@@ -1,7 +1,7 @@
 let fruits = [
-  { id: 1, name: "Apple", price: 250 ,qty : 2, status : "pending" },
-  { id: 2, name: "Orange", price: 100, qty : 2,status : "pending"  },
-  { id: 3, name: "Mango", price: 80 ,qty : 2, status : "pending" },
+  { id: 1, name: "Apple", price: 2 ,qty : 2, status : "pending" },
+  { id: 2, name: "Orange", price: 3, qty : 2,status : "pending"  },
+  { id: 3, name: "Mango", price: 4 ,qty : 2, status : "pending" },
 ];
 
 let updatedFruits = fruits.map(element => ({...element , total : element.price * element.qty , status : "completed" , }));

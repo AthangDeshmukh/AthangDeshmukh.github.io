3c2f7345d9367c0f45da278a04a4c26ecbfaa824
AthangDeshmukh <athang.deshmukh@gmail.com>
2025-05-17 20:56:08 +0530

    update

--- a17/index.js
+++ a17/index.js
@@ -1,13 +1,26 @@
 function add(x, y) {
-  return Promise.resolve(x + y);
+  return new Promise((resolve, reject) => {
+    const sum = x + y;
+    if (sum < 5) {
+      reject("sum is less than 5");
+    } else {
+      resolve(sum);
+    }
+  });
 }
+
 async function sqr(a) {
-    return a*a;
+  return a * a;
 }
-async function main(){
-    let sum-await add(5, 10);
+
+async function main() {
+  try {
+    let sum = await add(5, 10);
     let result = await sqr(sum);
     console.log(result);
-    
+  } catch (err) {
+    console.log(err);
+  }
 }
+
 main();
